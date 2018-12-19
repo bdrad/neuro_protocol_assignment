7d47f43a12708ab8902a9102cf8219fadcd59419
Scott Werwath <swerwath@berkeley.edu>
2018-12-19 14:44:23 -0500

    Classification code

---
+++ main.py
@@ -0,0 +1,69 @@
+from random import shuffle
+from csv import DictReader, DictWriter
+import argparse
+
+from models import get_log_reg_model
+
+CONCAT_ORDER = True
+
+# Returns list of tuples of form (Report Text, Image Order, Protocol Category, Protocol Specific)
+def read_csv(in_path):
+    csv_reader = DictReader(open(in_path, 'r'))
+    valid_entries = filter(lambda entry: entry['PROTOCOL CATEGORY'] != '' and entry['PROTOCOL CATEGORY'] != 'NA', csv_reader)
+    if CONCAT_ORDER:
+        return [(e['Final Text'] + " " + e['Image Order'], e['Image Order'], e['PROTOCOL CATEGORY'], e['PROTCOL SPECIFIC']) for e in valid_entries]
+    else:
+        return [(e['Final Text'], e['Image Order'], e['PROTOCOL CATEGORY'], e['PROTCOL SPECIFIC']) for e in valid_entries]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', type=str)
+    parser.add_argument('output_path', type=str)
+    args = parser.parse_args()
+
+    
+    # Read data and build specific -> general lookup
+    data = read_csv(args.input_path)
+    spec_to_gen = {}
+    for _, _, gen, spec in data:
+        if spec not in spec_to_gen.keys():
+            spec_to_gen[spec] = gen
+
+    train_ratio = 0.8
+    split_point = int(len(data) * train_ratio)
+    shuffle(data)
+    train_data, test_data = data[:split_point], data[split_point:]
+    train_reports, _, _, train_labels = zip(*train_data)
+    test_reports, _, _, test_labels = zip(*test_data)
+
+    # Try predicting general label
+    logreg_model = get_log_reg_model(train_reports, train_labels)
+    print(logreg_model.score(test_reports, test_labels))
+
+    # Train a model on all data
+    reports, _, _, labels = zip(*data)
+    logreg_model = get_log_reg_model(reports, labels)
+
+    # Replace entries
+    csv_reader = DictReader(open(args.input_path, 'r'))
+    entries = []
+    dict_keys = csv_reader.fieldnames + ['Manually Annotated']
+    for entry in csv_reader:
+        if entry['PROTOCOL CATEGORY'] != '':
+            entry['Manually Annotated'] = 1
+        else:
+            if CONCAT_ORDER:
+                pred = logreg_model.predict([entry['Final Text'] + " " + entry['Image Order']])[0]
+            else:
+                pred = logreg_model.predict([entry['Final Text']])[0]
+            entry['PROTCOL SPECIFIC'] = pred
+            entry['PROTOCOL CATEGORY'] = spec_to_gen[pred]
+            entry['Manually Annotated'] = 0
+        entries.append(entry)
+    
+    writer = DictWriter(open(args.output_path, 'w'), fieldnames=dict_keys)
+    writer.writeheader()
+    writer.writerows(entries)
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,33 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.base import TransformerMixin
+
+
+class Preprocessor(TransformerMixin):
+    def _process(self, report):
+        report = report.lower()
+        tokens = word_tokenize(report)
+
+        to_remove = set(stopwords.words('english')) | set(",.;'()!?")
+        tokens = filter(lambda t: t not in to_remove, tokens)
+
+        return " ".join(tokens)
+
+    def transform(self, X, *_):
+        return list(map(self._process, X))
+    
+    def fit(self, *_):
+        return self
+
+def get_log_reg_model(data, labels):
+    text_clf = Pipeline([
+        ('preproc', Preprocessor()),
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', LogisticRegression())
+    ])
+    text_clf.fit(data, labels)
+    return text_clf
